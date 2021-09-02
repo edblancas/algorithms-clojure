03a6707f1ffa1dedecbf6f7780ac211318c3c748
Daniel Blancas <edblancas@gmail.com>
2021-09-01 22:20:38 -0500

    init repo

---
+++ src/edblancas/algo/ds.clj
@@ -0,0 +1,13 @@
+(ns edblancas.algo.ds)
+
+(defn reverse-cmp [a b]
+  (compare b a)
+  )
+
+(comment
+  (defn reverse-cmp [a b]
+    (compare b a))
+
+  (let [some-var [1 2 3]
+        other-some-var [4 5 6]])
+  )
